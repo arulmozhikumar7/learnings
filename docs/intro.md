ad0abacb3fab214f997c48bce7154a1224e5cf33
arulmozhikumar7 <arulmozhikumar7@gmail.com>
2025-07-22 21:29:27 +0530

    chore: initial Docusaurus setup

---
+++ docs/intro.md
@@ -0,0 +1,32 @@
+---
+sidebar_position: 1
+---
+
+# 👋 Hey, I'm Arulmozhikumar K
+
+This is my personal space where I write down stuff I’m learning — tech, tools, ideas — basically anything I want to remember or figure out better.
+
+Some things might be right, some might be wrong. It’s all part of the process.
+
+---
+
+## 📓 What you'll find here
+
+- Notes on concepts I'm exploring
+- Code snippets and walkthroughs
+- Things I broke (and how I fixed them)
+- Just general learnings, mostly tech
+
+> These aren’t polished tutorials. I’m just thinking out loud.
+
+---
+
+## 🔗 Connect with me
+
+- [LinkedIn](https://www.linkedin.com/in/arulmozhikumar7)  
+- [GitHub](https://github.com/arulmozhikumar7)
+
+---
+
+That’s it. Nothing fancy.  
+I'll keep updating this as I go ✌️
---
+++ docusaurus.config.ts
@@ -0,0 +1,77 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Hii',
+  tagline: 'Learning one byte at a time',
+  favicon: 'img/logo.png',
+
+  future: {
+    v4: true, 
+  },
+
+  url: 'https://docs.arulmozhikumar.dev', 
+  baseUrl: '/',
+
+  organizationName: 'arulmozhikumar7', 
+  projectName: 'learnings', 
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          editUrl: 'https://github.com/arulmozhikumar7/learnings/edit/main/docs/',
+        },
+        blog: false, 
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    image: 'img/logo.png',
+    navbar: {
+      title: 'Arulmozhikumar',
+      logo: {
+        alt: 'Arulmozhikumar Logo',
+        src: 'img/logo.png',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Docs',
+        },
+        {
+          href: 'https://github.com/arulmozhikumar7',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Code, break, repeat. © ${new Date().getFullYear()} Arul`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
