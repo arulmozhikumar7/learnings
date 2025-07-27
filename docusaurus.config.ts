eb6d288a69f16f7221f76bfef13c78997c527f40
arulmozhikumar7 <arulmozhikumar7@gmail.com>
2025-07-27 20:35:51 +0530

    fix: title

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'Hii',
+  title: 'Arulmozhikumar',
   tagline: 'Learning one byte at a time',
   favicon: 'img/logo.png',
 
